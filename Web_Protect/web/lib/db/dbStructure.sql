223a4215c21c73b74a053c167d8f92e230a11efc
linkstrifer <linkstrifer@outlook.com>
2013-08-21 16:34:15 -0500

    DB structure

---
+++ Web_Protect/web/lib/db/dbStructure.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.05 
+MySQL - 5.6.13-log : Database - webapplication
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`webapplication` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `webapplication`;
+
+/*Table structure for table `schedule` */
+
+DROP TABLE IF EXISTS `schedule`;
+
+CREATE TABLE `schedule` (
+  `id` int(255) NOT NULL,
+  `names` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `cellphone` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
